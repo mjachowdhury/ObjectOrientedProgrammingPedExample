4d114398c40ca6ae71bb8c6107b6aa330ccb297c
mjachowdhury <study.computing2016@gmail.com>
2017-09-12 01:53:26 +0100

    added while loop on it
    added while loop on it

--- ObjectOrientedProgrammingForPedestrians/src/ifExample/IfExampleApp.java
+++ ObjectOrientedProgrammingForPedestrians/src/ifExample/IfExampleApp.java
@@ -10,12 +10,18 @@ public class IfExampleApp {
 		String password;
 		System.out.println("Please enter your password :");
 		password = input.nextLine();
-		
-		if(password.equals("javaProgram")){
+		while(!password.equals("javaProgram")){
+			System.out.println("Try again.");
+			password = input.nextLine();
+		}
+		System.out.println("Correct!!");
+		//both will work
+		//if(password.equals("javaProgram")){
+			/*if(password.equalsIgnoreCase("javaProgram")){
 			System.out.println("Password is corret");
 		}
 		else{
-			System.out.println("Wrong password. Sorry");
-		}
+			System.out.println("Wrong password.Sorry");
+		}*/
 	}
 }
